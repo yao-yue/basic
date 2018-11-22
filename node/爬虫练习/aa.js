0c50602b14756311df091127dd9325cbdc0f8071
longbits <2316570512@qq.com>
2018-11-22 17:15:57 +0800

    万恶啊

--- node/爬虫练习/aa.js
+++ node/爬虫练习/aa.js
@@ -1,4 +1,5 @@
 const puppeteer = require('puppeteer');
+const cheerio = require('cheerio');
 
 let scrape = async () => {
   const browser = await puppeteer.launch({ headless: false });
@@ -7,37 +8,34 @@ let scrape = async () => {
   const length = await page.evaluate(() => {
     return document.querySelectorAll('#sectionLeft > li').length
   })
- 
-  for(let i = 1; i<=length; i++) {
-    await page.click(`#sectionLeft > li:nth-child(${i})`)
+
+  //有一处坑  不知道为什么.product:nth-child(2)这个索引是从2开始计算
+   //找到问题所在了  :nth-of-type()
+  // await page.hover(`#section > div > dl:nth-child(2) > .product:nth-child(2)`)
+
+  for(let i = 1; i<=length+1; i++) {
+    
     let len = await page.evaluate((i) => {
       return document.querySelectorAll(`.scroll-tab-content > dl:nth-child(${i}) > .product`).length
     }, i)
     console.log(len)
     console.log('---------------')
-    for(let j = 1; j<=len; j++){
-      console.log(j)
-      await page.click(`.scroll-tab-content > dl:nth-child(${i})>.product:nth-child(${j})`);
+    for(let j = 2; j<=len; j++){
+      await page.waitFor(`#section > div > dl:nth-child(${i}) > .product:nth-child(${j}`)
+      await page.hover(`#section > div > dl:nth-child(${i}) > .product:nth-child(${j})`);
     }
-    setTimeout(() => {},600);
+    
   }
+await page.waitFor(4000);  
 
-
-  // await page.evaluate(() => {
-  //   let sc = document.querySelector('#section > .scroll-tab-content');
-  //   while(scrollY < 40000){
-  //     sc.style.transform = `translate(0px, -${scrollY}px) translateZ(0px)`;
-  //     scrollY += 500;
-  //   }
-  // })
-await page.waitFor(10000);  
-
-
-  const result = await page.evaluate(() => {
-     return  [...document.querySelectorAll('.scroll-tab-content > dl > .product > .desc > p')].map(item => {
-      return item.innerText
-    })
-  });
+  let $ = cheerio.load(page)
+  const result = await page.evaluate(($) => {
+    let a = $(`#section > div > dl:nth-child(2) > .product:nth-child(2)`).innerText
+    return a
+    //  return  [...document.querySelectorAll('.scroll-tab-content > dl > .product > .desc > p')].map(item => {
+    //   return item.innerText
+    // })
+  }, $);
   browser.close();
   return result;
 };
@@ -45,3 +43,4 @@ await page.waitFor(10000);
 scrape().then((value) => {
   console.log(value); // Success!
 });
+
---
+++ node/爬虫练习/bb.js
@@ -0,0 +1,57 @@
+const puppeteer = require('puppeteer');
+
+let scrape = async () => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.goto('https://m.4008823823.com.cn/');
+  const length = await page.evaluate(() => {
+    return document.querySelectorAll('#sectionLeft > li').length
+  })
+
+  //有一处坑  .product:nth-child(2)这个的索引是从2开始计数
+   //找到问题所在了  :nth-of-type()
+
+  for(let i = 1; i<=length; i++) {
+    let len = await page.evaluate((i) => {
+      return document.querySelectorAll(`.scroll-tab-content > dl:nth-of-type(${i}) > .product`).length
+    }, i)
+    console.log(len)
+    console.log('---------------')
+    for(let j = 1; j<=len; j++){
+    //   await page.click(`#sectionLeft > li:nth-of-type(${i})`)
+      await page.waitFor(`#section > div > dl:nth-of-type(${i}) > .product:nth-of-type(${j}`)
+      await page.hover(`#section > div > dl:nth-of-type(${i}) > .product:nth-of-type(${j})`);
+    }  
+  }
+await page.waitFor(2000);  
+
+
+  const result = await page.evaluate(() => {
+    var $ = window.$
+     let menu = [...document.querySelectorAll('.scroll-tab-content > dl')].map(item => {
+         let dt = item.firstChild.innerText;
+         let product = [...item.getElementsByClassName('product flex')].map(i => {
+             let arr = {
+                src : i.firstChild.src,
+                p : i.firstChild.alt,
+                money : $('.desc > .price > .showPrice > span > .fcm').innerText
+             }
+            //  let p = i.getElementsByClassName('.desc .p').innerText;
+            //  let money = i.getElementsByClassName('.desc > .price > .showPrice > span > .fcm').innerText;
+             return $('.desc > .price > .showPrice > span > .fcm').innerText
+                              
+         });
+      return  {
+          dt: dt,
+          product: product
+      }
+    })
+    return menu;
+});
+  browser.close();
+  return result;
+};
+
+scrape().then((value) => {
+  console.log(value); // Success!
+});
