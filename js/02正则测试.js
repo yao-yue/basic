5fe3dee144e19b981e78ec1e50137cba50b1fddb
hbw-cloud <2316570512@qq.com>
2018-12-17 02:28:03 +0800

    正则解决实际问题

--- js/02正则测试.js
+++ js/02正则测试.js
@@ -1,7 +1,13 @@
 let  p = '<p>1.这个有免运费的吗？没有</p><p>2.这个有免运费的吗？没有</p><p>3.这个有免运费的吗？没有</p>';
 let List = []
+let res = []
 List = p.replace(/[\r\t\n]/g, "").match(/(\<p\>)(.*?)(\<\/p\>)/g)
 for(let item of List) {
-    
+    res.push(item.slice(5,-4))
 }
-console.log(List);
+console.log(res);
+
+// let a = "1234 5678"
+// let reg = /(\d{4}) (\d{4})/
+// let result = a.replace(reg, "$2 $1")
+// console.log(result)
