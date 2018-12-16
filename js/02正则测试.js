75a1c2cb1722a646e41397bc32915b2c3c6ee1cc
hbw-cloud <2316570512@qq.com>
2018-12-17 00:02:24 +0800

    正则之路  快速上手

--- js/02正则测试.js
+++ js/02正则测试.js
@@ -1,2 +1,7 @@
 let  p = '<p>1.这个有免运费的吗？没有</p><p>2.这个有免运费的吗？没有</p><p>3.这个有免运费的吗？没有</p>';
-let reg = /(<p><p/>)/;
+let List = []
+List = p.replace(/[\r\t\n]/g, "").match(/(\<p\>)(.*?)(\<\/p\>)/g)
+for(let item of List) {
+    
+}
+console.log(List);
